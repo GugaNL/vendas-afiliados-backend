b97d75fa4731f2424c4dfc80ac623f5232338347
Gustavo Lucena <gustavo.lucena@compasso.com.br>
2023-02-16 17:10:37 -0300

    remove auth on list categories

--- src/routers/category.route.js
+++ src/routers/category.route.js
@@ -4,7 +4,7 @@ const CategoryController = require("../controllers/category.controller");
 const { verifyJWT } = require("../middlewares/authorizator");
 
 router.post("/novo", CategoryController.create);
-router.get("/lista", verifyJWT, CategoryController.listCategories);
+router.get("/lista", CategoryController.listCategories);
 router.get("/:id", CategoryController.findCategory);
 router.get("/busca/:numero",  CategoryController.findCategory);
 router.put("/:id", verifyJWT, CategoryController.updateCategory);
